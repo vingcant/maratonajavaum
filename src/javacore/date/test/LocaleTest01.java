2c3aa69dd8682f77ba93d41c2b632e936ca706aa
vingcant <kaikecantuario@gmail.com>
2025-06-15 20:14:07 -0300

    datas

--- src/javacore/date/test/LocaleTest01.java
+++ src/javacore/date/test/LocaleTest01.java
@@ -8,12 +8,17 @@ public class LocaleTest01 {
     public static void main(String[] args) {
         Locale localeBrazil = new Locale("pt", "br");
         Locale localePortugal = new Locale("pt", "pt");
+        Locale localeEngland = new Locale("en", "EN");
 
         Calendar calendar = Calendar.getInstance();
 
-        DateFormat.getDateInstance(DateFormat.FULL, localeBrazil);
-        DateFormat.getDateInstance(DateFormat.FULL, localePortugal);
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeBrazil);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localePortugal);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeEngland);
 
+        System.out.println(df1.format(calendar.getTime()));
+        System.out.println(df2.format(calendar.getTime()));
+        System.out.println(df3.format(calendar.getTime()));
         System.out.println(localeBrazil.getDisplayCountry(localePortugal));
     }
 }
